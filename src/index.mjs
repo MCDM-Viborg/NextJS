95c4b89886284d2ba274dd21add6b721c1bd7151
Anders Christensen <anders@medieskolerne.dk>
2023-09-02 13:57:35 +0200

    Paths

--- src/index.mjs
+++ src/index.mjs
@@ -1,19 +1,25 @@
 const menu = {
 
-    tmpl : () => {
+
+
+    tmpl : (location) => {
+
+        let path = (location.host === 'mcdm-viborg.github.io') ? location.origin + '/NextJS/' : '/';
+
         return `<nav class="navigation">
-            <a href="../">Forside</a>
-            <a href="./video_01.html">Video 01</a>
-            <a href="./video_02.html">Video 02</a>
-            <a href="./video_03.html">Video 03</a>
-            <a href="./video_04.html">Video 04</a>
+            <a href="${path}">Forside</a>
+            <a href="${path}video/video_01.html">Video 01</a>
+            <a href="${path}video/video_02.html">Video 02</a>
+            <a href="${path}video/video_03.html">Video 03</a>
+            <a href="${path}video/video_04.html">Video 04</a>
         </nav>`
     },
 
     init : () => {
 
+
         const body = document.querySelector('body');
-        body.insertAdjacentHTML('beforebegin', menu.tmpl())
+        body.insertAdjacentHTML('beforebegin', menu.tmpl(location))
 
     }
 
